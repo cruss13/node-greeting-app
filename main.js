7b44060b9732b8b9f8d483ea205d77c018e58e86
Corey Russ <corey.j.russ@gmail.com>
2018-04-23 20:39:14 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const greetings = require('./greeting.js');
+const valedictions = require('./valediction.js');
+
+greetings.greetingEnglish();
+
+valedictions.valedictionEnglish();
+
+greetings.greetingJapanese();
+
+valedictions.valedictionJapanese();
+
+greetings.greetingSpanish();
+
+valedictions.valedictionSpanish();
+
+greetings.greetingIcelandic();
+
+valedictions.valedictionIcelandic();
---
+++ valediction.js
@@ -0,0 +1,15 @@
+exports.valedictionEnglish = function() {
+  console.log('Good bye');
+}
+
+exports.valedictionJapanese = function() {
+  console.log('Sayonara');
+}
+
+exports.valedictionSpanish = function() {
+  console.log('Adios');
+}
+
+exports.valedictionIcelandic = function() {
+  console.log('Bless');
+}
